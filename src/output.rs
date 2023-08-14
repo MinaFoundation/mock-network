adab33e94e03884d70ebc9e034d01666cc025cf5
Quantifier <quantifier-tech@protonmail.com>
2023-08-14 14:46:17 +0000

    (output) Modularize

--- src/output.rs
+++ src/output.rs
@@ -1,99 +1,103 @@
-use serde::Serialize;
+pub mod network {
+    use serde::Serialize;
+    
+    #[derive(Debug, Serialize)]
+    pub struct Create {
+        pub network_id: String,
+        pub node_map: std::collections::HashMap<String, super::node::Info>,
+    }
 
-#[derive(Debug, Serialize)]
-pub struct NetworkCreate {
-    pub network_id: String,
-    pub node_map: std::collections::HashMap<String, NodeInfo>,
+    #[derive(Debug, Serialize)]
+    pub struct Start {
+        pub network_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Stop {
+        pub network_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Status {
+        pub network_id: String,
+        pub status: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Delete {
+        pub network_id: String,
+    }
 }
 
-#[derive(Debug, Serialize)]
-pub struct NetworkDelete {
-    pub network_id: String,
+pub mod node {
+    use serde::Serialize;
+
+    #[derive(Debug, Serialize)]
+    pub struct Info {
+        pub graphql_uri: Option<String>,
+        pub network_keypair: Option<String>,
+        pub node_type: Type,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub enum Type {
+        Archive,
+        BlockProducer,
+        Seed,
+        SnarkCoordinator,
+        SnarkWorker,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Start {
+        pub fresh_state: bool,
+        pub git_commit: String,
+        pub network_id: String,
+        pub node_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Stop {
+        pub network_id: String,
+        pub node_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct ArchiveData {
+        pub data: String,
+        pub network_id: String,
+        pub node_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct MinaLogs {
+        pub logs: String,
+        pub network_id: String,
+        pub node_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct PrecomputedBlocks {
+        pub blocks: String,
+        pub network_id: String,
+        pub node_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct ReplayerLogs {
+        pub logs: String,
+        pub network_id: String,
+        pub node_id: String,
+    }
 }
 
-#[derive(Debug, Serialize)]
-pub struct NetworkStart {
-    pub network_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct NetworkStop {
-    pub network_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct NetworkStatus {
-    pub network_id: String,
-    pub status: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct NodeInfo {
-    pub graphql_uri: Option<String>,
-    pub network_id: String,
-    pub network_keypair: Option<String>,
-    pub node_id: String,
-    pub node_type: NodeType,
-}
-
-#[derive(Debug, Serialize)]
-pub enum NodeType {
-    Archive,
-    BlockProducer,
-    Seed,
-    SnarkCoordinator,
-    SnarkWorker,
-}
-
-#[derive(Debug, Serialize)]
-pub struct NodeStart {
-    pub fresh_state: bool,
-    pub git_commit: String,
-    pub network_id: String,
-    pub node_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct NodeStop {
-    pub network_id: String,
-    pub node_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct ArchiveData {
-    pub data: String,
-    pub network_id: String,
-    pub node_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct MinaLogs {
-    pub logs: String,
-    pub network_id: String,
-    pub node_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct PrecomputedBlocks {
-    pub blocks: String,
-    pub network_id: String,
-    pub node_id: String,
-}
-
-#[derive(Debug, Serialize)]
-pub struct ReplayerLogs {
-    pub logs: String,
-    pub network_id: String,
-    pub node_id: String,
-}
-
-#[derive(Debug, Serialize)]
+#[derive(Debug, serde::Serialize)]
 pub struct Error {
     pub network_id: String,
 }
 
 macro_rules! impl_display {
-    ($name:ident) => {
+    ($name:path) => {
         impl std::fmt::Display for $name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 write!(f, "{}", serde_json::to_string_pretty(self).unwrap())?;
@@ -103,13 +107,13 @@ macro_rules! impl_display {
     };
 }
 
-impl_display!(NetworkCreate);
-impl_display!(NetworkStart);
-impl_display!(NetworkStop);
-impl_display!(NetworkStatus);
-impl_display!(NodeStart);
-impl_display!(NodeStop);
-impl_display!(ArchiveData);
-impl_display!(MinaLogs);
-impl_display!(PrecomputedBlocks);
-impl_display!(ReplayerLogs);
+impl_display!(network::Create);
+impl_display!(network::Start);
+impl_display!(network::Stop);
+impl_display!(network::Status);
+impl_display!(node::Start);
+impl_display!(node::Stop);
+impl_display!(node::ArchiveData);
+impl_display!(node::MinaLogs);
+impl_display!(node::PrecomputedBlocks);
+impl_display!(node::ReplayerLogs);
